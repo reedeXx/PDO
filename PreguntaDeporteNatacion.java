62121e647a4f49a22d4819b4281dcf47dca48250
reedeXx <81296701+reedeXx@users.noreply.github.com>
2023-12-08 12:17:42 +0000

    Update PreguntaDeporteNatacion.java

--- PreguntaDeporteNatacion.java
+++ PreguntaDeporteNatacion.java
@@ -1,13 +1,26 @@
-public class PreguntaDeporteNatacion extends Pregunta{
+public class PreguntaDeporteNatacion extends PreguntaDeporte{
         public PreguntaDeporteNatacion(String texto , int puntuacion) {
             super(texto , puntuacion );
         }
 
+        public PreguntaDeporteNatacion( String texto, int puntuacion ,boolean respuesta ){
+
+            super(texto, puntuacion, respuesta);    
+
+        }
+
+        @Override
+        public void mostrarRespuesta() {
+            
+            super.mostrarRespuesta();
+    
+        }
+        
         @Override
         public int getPuntuacion() {
             puntuacionBase = super.getPuntuacion();
             return puntuacionBase + 10;
         }
 
-    }
+}
 
