704c7a666f5e32bc3e6357433c4472b28c1a3658
reedeXx <81296701+reedeXx@users.noreply.github.com>
2023-12-08 20:16:10 +0000

    Update Fichero.java

--- Fichero.java
+++ Fichero.java
@@ -20,6 +20,7 @@ public class Fichero {
         }
         return listaPreguntas;
     }
+
 /*
     // Método para guardar el estado del juego en un archivo de objetos
     public static void guardarEstadoJuego(String nombreArchivo, InformacionJuego informacionJuego) {
@@ -30,7 +31,8 @@ public class Fichero {
         }
     }
 */
-    /* Método para cargar el estado del juego desde un archivo de objetos
+
+/* Método para cargar el estado del juego desde un archivo de objetos
     public static InformacionJuego cargarEstadoJuego(String nombreArchivo) {
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(nombreArchivo))) {
             return (InformacionJuego) ois.readObject();
@@ -39,7 +41,7 @@ public class Fichero {
         }
         return null;
     }
-    */
+*/
     
     /* Método privado para convertir una línea en una pregunta private static Pregunta parsearLineaAPregunta(String linea) {
         String[] partes = linea.split(";");
@@ -56,6 +58,62 @@ public class Fichero {
         }
     }*/
 
+
+    private static String obtenerIniciales( String nombreCompleto ){
+
+        StringBuilder iniciales = new StringBuilder();
+
+        if (nombreCompleto != null && !nombreCompleto.isEmpty()) {
+            String[] partesNombre = nombreCompleto.split("\\s+"); // Dividir por espacios en blanco
+
+            for (String parte : partesNombre) {
+                if (!parte.isEmpty()) {
+                    iniciales.append(parte.charAt(0));
+                }
+            }
+        }
+
+        return iniciales.toString().toUpperCase();        
+
+    }
+
+    public static void guardarEstadoJuego(Jugador j ) {
+        String nombreArchivo = "pootrivia_jogo_" + j.getFecha() + "_" +  obtenerIniciales( j.getNombre() ) + ".dat";
+ // Deserialización de varios objetos
+        try (ObjectOutputStream salida  = new ObjectOutputStream(new FileOutputStream(nombreArchivo))) {
+
+            salida.writeObject(j.getFecha());
+            salida.writeObject(j.getNombre());
+            salida.writeObject(j.getPreguntasAcertadas());     
+        
+
+            System.out.println("Objetos serializados correctamente.");
+                      
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Jugador cargarJugador(String nombrearchivp){
+
+        try (ObjectInputStream entrada = new ObjectInputStream(new FileInputStream("personas.dat"))) {
+
+            String f = (String) entrada.readObject();
+            String n = (String) entrada.readObject();
+            List <Pregunta> pA = (List <Pregunta>) entrada.readObject();
+              
+            Jugador jdes = new Jugador( n , f , pA );
+
+            return jdes;
+
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+
+    }
+
     private static Pregunta parsearLineaAPregunta(String linea) {
         String[] partes = linea.split(";");
         if (partes.length >= 2) {
@@ -67,8 +125,10 @@ public class Fichero {
                 String[] textoRespuesta = texto.split("::A::"); 
                 List<String> opciones = Arrays.asList(Arrays.copyOfRange(partes, 1, partes.length));     
                 int inicio = Math.max(0, opciones.size() - 3);
-                int fin = opciones.size();                                        
-                return new PreguntaArtes(textoRespuesta[1],  5, partes[partes.length - 1] , opciones, opciones.subList(inicio, fin) );   
+                int fin = opciones.size();       
+                List<String> subListaSerializable = new ArrayList<>(opciones.subList(inicio, fin));
+
+                return new PreguntaArtes(textoRespuesta[1],  5, partes[partes.length - 1] , opciones, subListaSerializable );   
             } else if (linea.contains("::C::")) {
                 String[] textoRespuesta = texto.split("::C::");           
                 List<String> opciones = Arrays.asList(Arrays.copyOfRange(partes, 1, partes.length));         
@@ -86,7 +146,6 @@ public class Fichero {
             } else if (linea.contains("::S::")) {
                 String[] textoRespuesta = texto.split("::S::");  
                 List<String> opciones = Arrays.asList(Arrays.copyOfRange(partes, 1, partes.length));
-                //System.out.println(partes[partes.length - 1]);
                 return new PreguntaDeporteSki(textoRespuesta[1], 5, partes[partes.length - 1].contains("Verdadero") );              
                 
             } else {
