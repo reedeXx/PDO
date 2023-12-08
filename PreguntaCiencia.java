8f41f030e21a299b89ad3f0c3b6d1dfcc801ddfe
reedeXx <81296701+reedeXx@users.noreply.github.com>
2023-12-08 12:22:37 +0000

    Update PreguntaCiencia.java

--- PreguntaCiencia.java
+++ PreguntaCiencia.java
@@ -4,6 +4,12 @@ public class PreguntaCiencia extends Pregunta{
 
     private List<String> opFac;
     private List<String> opDif;    
+    
+
+
+    public PreguntaCiencia(String texto, int pontuacaoBase ) {
+        super(texto, pontuacaoBase);
+    }
 
     public PreguntaCiencia(String texto, int pontuacaoBase, List<String> opcoesFaceis, List<String> opcoesDificeis) {
         super(texto, pontuacaoBase);
@@ -18,6 +24,19 @@ public class PreguntaCiencia extends Pregunta{
     public List<String> getOpDif() {
         return opDif;
     }
+
+    @Override
+    public void mostrarRespuesta() {
+        
+        
+        System.out.println("Opciones: \n");
+        for ( int i=0 ; i < opDif.size() ; i++ )
+            System.out.println(opDif.get(i));
+        for ( int i=0 ; i < opFac.size() ; i++ )
+            System.out.println(opFac.get(i));
+            
+    }
+
 
     @Override
     public int getPuntuacion() {
