eb4f96d1fea5031ce8aa7b55b6625c607b7f98a8
reedeXx <81296701+reedeXx@users.noreply.github.com>
2023-12-08 12:37:07 +0000

    Update PreguntaDeporteSki.java

--- PreguntaDeporteSki.java
+++ PreguntaDeporteSki.java
@@ -3,7 +3,20 @@ public class PreguntaDeporteSki extends PreguntaDeporte{
     public PreguntaDeporteSki(String texto , int puntuacion) {
         super(texto , puntuacion );
     }
+    
+    public PreguntaDeporteSki( String texto, int puntuacion ,boolean respuesta ){
 
+        super(texto, puntuacion, respuesta);    
+
+    }
+
+    @Override
+    public void mostrarRespuesta() {
+        
+        super.mostrarRespuesta();
+
+    }
+   
     @Override
     public int getPuntuacion() {
         puntuacionBase = super.getPuntuacion();
