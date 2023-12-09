2ef95fdea67aa27a0f236053327b6c41f7a32b52
Paula170 <72363320+Paula170@users.noreply.github.com>
2023-12-09 15:12:18 +0000

    Update Pregunta.java

--- Pregunta.java
+++ Pregunta.java
@@ -1,58 +1,25 @@
 import java.util.List;
-import java.util.Random;
 
 public class Pregunta {
 
     protected String texto;
     protected int puntuacionBase;
-    List<String> opc;
-    String sol;
-    String categoria;
 
-    public Pregunta(String texto, int pontuacaoBase, List<String> opc, String sol, String categoria) {
-        this.texto=texto;
-        this.puntuacionBase=pontuacaoBase;
-        this.opc=opc;
-        this.sol=sol;
-        this.categoria=categoria;
-
+    public Pregunta(String texto, int pontuacaoBase) {
+        this.texto = texto;
+        this.puntuacionBase = pontuacaoBase;
     }
 
     public String getTexto() {
         return texto;
     }
 
-    public String getCategoria() {
-        return this.categoria;
-    }
-
-    public void mezclarOpciones() {
-        Random random = new Random();
-        for (int i = opc.size()-1; i > 0; i--) {
-            int j = random.nextInt(i + 1);
-
-
-            String temp = opc.get(i);
-            opc.set(i, opc.get(j));
-            opc.set(j, temp);
-        }
-    }
-
-
-
-    public int getCantidadOpciones() {
-        return opc.size();
-    }
-
     public int getPuntuacion() {
         return puntuacionBase;
     }
 
-    public String getRespuesta(){
-        return sol;
+    public void mostrarRespuesta(){
+
     }
 
-    public List<String> getOpciones() {
-        return this.opc;
-    }
 }
