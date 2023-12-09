b81322c6f29f30f5979efb7cf9d10674622cf980
Paula170 <72363320+Paula170@users.noreply.github.com>
2023-12-09 14:47:09 +0000

    Update PreguntaCiencia.java

--- PreguntaCiencia.java
+++ PreguntaCiencia.java
@@ -1,46 +1,30 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class PreguntaCiencia extends Pregunta{
 
-    private List<String> opFac;
-    private List<String> opDif;    
-    
 
 
-    public PreguntaCiencia(String texto, int pontuacaoBase ) {
-        super(texto, pontuacaoBase);
+    public PreguntaCiencia(String texto, int pontuacaoBase,List<String> opciones, String sol, String categoria ) {
+        super(texto, pontuacaoBase, opciones, sol,  categoria );
+
+    }
+    public String getTexto() {
+        return super.getTexto();
     }
 
-    public PreguntaCiencia(String texto, int pontuacaoBase, List<String> opcoesFaceis, List<String> opcoesDificeis) {
-        super(texto, pontuacaoBase);
-        this.opFac = opcoesFaceis;
-        this.opDif = opcoesDificeis;
-    }
+    public String getCategoria(){return super.getCategoria();}
 
-    public List<String> getOpFac() {
-        return opFac;
-    }
-
-    public List<String> getOpDif() {
-        return opDif;
-    }
-
-    @Override
-    public void mostrarRespuesta() {
-        
-        
-        System.out.println("Opciones: \n");
-        for ( int i=0 ; i < opDif.size() ; i++ )
-            System.out.println(opDif.get(i));
-        for ( int i=0 ; i < opFac.size() ; i++ )
-            System.out.println(opFac.get(i));
-            
-    }
-
-
-    @Override
     public int getPuntuacion() {
-        return puntuacionBase + 5;
+        return super.getPuntuacion();
+    }
+
+    public String getRespuesta(){
+        return super.getRespuesta();
+    }
+
+    public List<String> getOpciones() {
+        return super.getOpciones();
     }
 
 }
