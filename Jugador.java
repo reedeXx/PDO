5050c7939a8a2e1e3274f14865f5dc78fb23b512
reedeXx <81296701+reedeXx@users.noreply.github.com>
2023-12-09 17:02:28 +0000

    Update Jugador.java

--- Jugador.java
+++ Jugador.java
@@ -1,14 +1,19 @@
+import java.io.Serializable;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
-public class Jugador {
+public class Jugador implements Serializable{
 
     private String nombre;
-    private int puntuacion;
+    private int puntuacion=0;
     private String FechaHora;
+    private List <Pregunta> preguntasAcertadas;
 
     public Jugador( String nombre ) {
         
+        this.preguntasAcertadas = new ArrayList<>();
         this.nombre = new String(nombre);
         Date fechaHoraActual = new Date();
         SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd HH:mm");
@@ -16,21 +21,74 @@ public class Jugador {
 
     }
 
+    public Jugador( String nombre , String fecha , List<Pregunta> pa ){
+
+        this.preguntasAcertadas = pa;
+        this.nombre = nombre;
+        this.FechaHora = fecha;
+
+    }
+
     public String getNombre() {
         return nombre;
     }
 
-    public void atualizarPuntuacion(int puntuacion) {
-        this.puntuacion += puntuacion;
+    public void setNombre( String n ) {
+        this.nombre = n;
+    }
+
+    public List <Pregunta> getPreguntasAcertadas(){
+
+        return this.preguntasAcertadas;
+
+    }
+
+    public void setPreguntasAcertadas( Pregunta p ){
+
+        this.preguntasAcertadas.add(p);
+
+    }
+
+
+    public void atualizarPuntuacion() {
+        
+        int p=0;
+        for (  int i=0 ; i < preguntasAcertadas.size() ; i++ ){
+
+            p += preguntasAcertadas.get(i).getPuntuacion();
+
+        }
+
+        this.puntuacion = p;
+
     }
 
     public int getPuntuacion() {
         return puntuacion;
     }
 
+    public String getFecha(){
+
+        return FechaHora;
+
+    }
+    
+    public void setFecha( String f ) {
+        this.FechaHora= f;
+    }
+
+    
     public String toString(){
 
-        return "Data e hora: " + this.FechaHora + "\nNome do jogador: " + this.nombre;
+        String cadena = "Data e hora: " + this.FechaHora + "\nNome do jogador: " + this.nombre;
+
+        for (  int i=0 ; i< this.preguntasAcertadas.size() ; i++ ){
+
+            cadena += preguntasAcertadas.get(i).getTexto();
+
+        }
+    
+        return cadena;
 
     }
 
